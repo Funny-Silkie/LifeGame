709e6ca496e201e3110f0ee9a3f0e90dbc4c6c9b
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-07-11 03:53:04 +0900

    Initialize

---
+++ LifeGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using Altseed2;
+
+namespace LifeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Engine.Initialize("Life Game", 960, 720)) return;
+            while (Engine.DoEvents()) Engine.Update();
+            Engine.Terminate();
+        }
+    }
+}
